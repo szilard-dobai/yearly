35e591a69daceacf706a072e120b59fbbd084784
Szilard DOBAI <dobai_szilard@yahoo.com>
2025-11-29 22:12:21 +0200

    feat: Reorder days in dropdown

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -29,13 +29,13 @@ export const MONTH_NAMES = [
 ] as const
 
 export const DAYS_OF_WEEK = [
-  { value: 0, label: 'Sunday' },
   { value: 1, label: 'Monday' },
   { value: 2, label: 'Tuesday' },
   { value: 3, label: 'Wednesday' },
   { value: 4, label: 'Thursday' },
   { value: 5, label: 'Friday' },
   { value: 6, label: 'Saturday' },
+  { value: 0, label: 'Sunday' },
 ] as const
 
 export const FLAG_SIZES = {
